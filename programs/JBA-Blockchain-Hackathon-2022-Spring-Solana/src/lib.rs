2face3ece4d74000c2f202a58750ce47ac5437cb
esper66 <65485839+esper66@users.noreply.github.com>
2022-03-13 22:41:55 +0900

    smartcontract test

--- programs/JBA-Blockchain-Hackathon-2022-Spring-Solana/src/lib.rs
+++ programs/JBA-Blockchain-Hackathon-2022-Spring-Solana/src/lib.rs
@@ -1,14 +1,28 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("AdENrMBJjcxGdbWspFtRjNELs467EmcecLgKdG9pcug4");
 
 #[program]
 pub mod jba_blockchain_hackathon_2022_spring_solana {
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+    pub fn initialize(ctx: Context<Initialize>, data: String) -> Result<()> {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.data = data;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 64 + 64)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+
+#[account]
+pub struct BaseAccount {
+    pub data: String,
+}
