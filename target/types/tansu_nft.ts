7c9063efc3287d6a970ce066a7bce02d6f4f0f3b
derodero24 <k.itsh20524abc@gmail.com>
2022-03-18 21:46:02 +0900

    add type file

---
+++ target/types/tansu_nft.ts
@@ -0,0 +1,133 @@
+export type TansuNft = {
+  "version": "0.1.0",
+  "name": "tansu_nft",
+  "instructions": [
+    {
+      "name": "initialize",
+      "accounts": [
+        {
+          "name": "tansu",
+          "isMut": true,
+          "isSigner": true
+        },
+        {
+          "name": "payer",
+          "isMut": true,
+          "isSigner": true
+        },
+        {
+          "name": "systemProgram",
+          "isMut": false,
+          "isSigner": false
+        }
+      ],
+      "args": [
+        {
+          "name": "originalToken",
+          "type": "publicKey"
+        },
+        {
+          "name": "innerTokens",
+          "type": {
+            "vec": "publicKey"
+          }
+        },
+        {
+          "name": "useFee",
+          "type": "f64"
+        }
+      ]
+    }
+  ],
+  "accounts": [
+    {
+      "name": "tansu",
+      "type": {
+        "kind": "struct",
+        "fields": [
+          {
+            "name": "originalToken",
+            "type": "publicKey"
+          },
+          {
+            "name": "innerTokens",
+            "type": {
+              "vec": "publicKey"
+            }
+          },
+          {
+            "name": "useFee",
+            "type": "f64"
+          }
+        ]
+      }
+    }
+  ]
+};
+
+export const IDL: TansuNft = {
+  "version": "0.1.0",
+  "name": "tansu_nft",
+  "instructions": [
+    {
+      "name": "initialize",
+      "accounts": [
+        {
+          "name": "tansu",
+          "isMut": true,
+          "isSigner": true
+        },
+        {
+          "name": "payer",
+          "isMut": true,
+          "isSigner": true
+        },
+        {
+          "name": "systemProgram",
+          "isMut": false,
+          "isSigner": false
+        }
+      ],
+      "args": [
+        {
+          "name": "originalToken",
+          "type": "publicKey"
+        },
+        {
+          "name": "innerTokens",
+          "type": {
+            "vec": "publicKey"
+          }
+        },
+        {
+          "name": "useFee",
+          "type": "f64"
+        }
+      ]
+    }
+  ],
+  "accounts": [
+    {
+      "name": "tansu",
+      "type": {
+        "kind": "struct",
+        "fields": [
+          {
+            "name": "originalToken",
+            "type": "publicKey"
+          },
+          {
+            "name": "innerTokens",
+            "type": {
+              "vec": "publicKey"
+            }
+          },
+          {
+            "name": "useFee",
+            "type": "f64"
+          }
+        ]
+      }
+    }
+  ]
+};
